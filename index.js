68cd6fd3e65dd2bfbc601d580cefb86de5e1c272
Jeremy Morrell <morrell.jeremy@gmail.com>
2013-09-03 23:07:14 -0500

    First implementation and tests.

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// Methods in the collection prototype that we won't expose
+var blacklistedMethods = [
+  "_onModelEvent", "_prepareModel", "_removeReference", "_reset", "add",
+  "initialize", "sync", "remove", "reset", "set", "push", "pop", "unshift",
+  "shift", "sort", "parse", "fetch", "create", "model", "off", "on",
+  "listenTo", "listenToOnce", "bind", "trigger", "once", "stopListening",
+];
+
+function proxyCollection(from, target, options) {
+
+  function updateLength() {
+    target.length = from.length;
+  }
+
+  function pipeEvents() {
+    var args = _.toArray(arguments);
+
+    // replace any references to `this._collection` with `this`
+    for (var i = 1; i < args.length; i++) {
+      if (args[i] && args[i].length && args[i].length === from.length) {
+        args[i] = this;
+      }
+    }
+
+    this.trigger.apply(this, args);
+  }
+
+  var methods = {};
+
+  _.each(_.functions(Backbone.Collection.prototype), function(method) {
+    if (!_.contains(blacklistedMethods, method)) {
+      methods[method] = function() {
+        return Backbone.Collection.prototype[method].apply(from, arguments);
+      };
+    }
+  });
+
+  _.extend(target, Backbone.Events, methods);
+
+  target.listenTo(from, 'all', pipeEvents);
+  target.listenTo(from, 'all', updateLength);
+
+  updateLength();
+  return target;
+}
+
+module.exports = proxyCollection;
+
